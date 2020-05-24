aad753cc43526970a46df0ba97b1f1b4efb852c3
Mumpfie <felix@boehm-esbeck.de>
2020-05-24 19:12:38 +0200

    increase version count

--- server/app/main.py
+++ server/app/main.py
@@ -22,7 +22,7 @@ def custom_openapi():
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="Ornithologists REST API",
-        version="1.0.1",
+        version="1.0.2",
         description="This API provides access to the user and IOC bird data for the Ornithologists app.",
         routes=app.routes
 
